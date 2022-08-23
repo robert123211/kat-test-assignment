95c8f5d2ea5cc4ef7dd91d62be0f76a4033b3bc5
robertkrikk <rkrikk@erply.com>
2022-08-23 15:30:48 +0300

    Added improved .gitignore, all dev dependencies to package.json & configurations for running Cypress 10 with cucumber & typescript

---
+++ config/production.config.ts
@@ -0,0 +1,32 @@
+import {defineConfig} from 'cypress';
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+import {addCucumberPreprocessorPlugin} from '@badeball/cypress-cucumber-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    'file:preprocessor',
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
+export default defineConfig({
+  defaultCommandTimeout: 15000,
+  watchForFileChanges: false,
+  e2e: {
+    baseUrl: 'https://katanamrp.com/login/',
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: '**/*.feature',
+    setupNodeEvents,
+  },
+});
